b46f4f614a0fbecf94b5233adf834e8f2a7f4020
Patrick Ferreira <patrickctrf@hotmail.com.br>
2018-03-28 10:26:13 -0300

    Add files via upload

---
+++ apower.cc
@@ -0,0 +1,58 @@
+#include <gpio.h>
+#include <alarm.h>
+
+#include <utility/ostream.h>
+#include <nic.h>
+	
+
+#include "auxiliar_power.h"
+
+
+static const unsigned int DUTY_CYCLE = 1; // %
+static const bool half_duty_cycle = true;
+
+using namespace EPOS;
+
+OStream cout;
+volatile int recebeu;//variável que indica se acabamos de receber um dado e que é zerada logo após enviarmos a mensagem designada.
+const unsigned int delay_time = 2000000;
+
+bool led_value;
+GPIO * led;
+
+void led_envio(){
+
+	led->set(1);//Sequencia para indicar dado sendo enviado.
+    Alarm::delay(delay_time/8);
+    led->set(0);
+    Alarm::delay(delay_time/8);
+    led->set(1);
+    Alarm::delay(delay_time/8);
+    led->set(0);
+    Alarm::delay(delay_time/8);
+}
+
+void led_recebimento(){
+
+	led->set(1);//Sequencia para indicar dado recebido.
+    Alarm::delay(delay_time);
+    led->set(0);
+    Alarm::delay(delay_time);
+    
+}
+
+
+
+int main()
+{
+    cout << "Hello main" << endl;
+    
+    while(1){
+    	led_envio();
+    	
+    	setPowerMode(pm_pm3);
+		
+	}
+
+    return 0;
+}
